aa223c42c08e22fe8690f6a71c323748d4f3908a
RYNEEER <ricardogabriel759@gmail.com>
2020-02-04 22:43:12 -0500

    Holi

--- app/src/main/java/facci/juliointriago/mongobd/rest/constants/ApiConstants.java
+++ app/src/main/java/facci/juliointriago/mongobd/rest/constants/ApiConstants.java
@@ -3,5 +3,5 @@ package facci.juliointriago.mongobd.rest.constants;
 public class ApiConstants {
     public static final String BASE_POST_URL = "https://backend-posts.herokuapp.com/";
 
-    public static final String MARKET_POST_ENDPOINT = "posts/";
+    public static final String MARKET_POST_ENDPOINT = "post/";
 }
